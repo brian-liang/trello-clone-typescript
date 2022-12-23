a02bba48a2420329397fe42165bfbcaa516d40c0
brian-liang <brian.j.liang@gmail.com>
2022-12-22 20:15:49 -0700

    011 - Column component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { CardContainer, ColumnContainer, ColumnTitle } from './styles';
+import { AppContainer} from './styles';
+import { Column } from "./components/Column"
 
 function App() {
   return (
-    <div className="App">
-      <ColumnContainer>
-        <ColumnTitle>Todo</ColumnTitle>
-        <CardContainer>First Item</CardContainer>
-        <CardContainer>Second Item</CardContainer>
-        <CardContainer>Third Item</CardContainer>
-      </ColumnContainer>
-    </div>
+    <AppContainer>
+      <Column text="Todo"/>
+    </AppContainer>
   );
 }
 
