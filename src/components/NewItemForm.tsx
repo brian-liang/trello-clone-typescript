441cfaad2fcd3d005cf62d4b8eb79185e93a659f
brian-liang <brian.j.liang@gmail.com>
2022-12-29 14:38:07 -0700

    015 - Auto focus on inputs

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,17 +1,23 @@
-import { useState } from "react"
-import { AddItemButton, NewItemFormContainer, NewItemInput } from "../styles"
+import { useState } from "react";
+import { AddItemButton, NewItemFormContainer, NewItemInput } from "../styles";
+import { useFocus } from "../utils/userFocus";
 
 type NewItemFormProps = {
-    onAdd(text: string): void
-}
+  onAdd(text: string): void;
+};
 
-export const NewItemForm = ({onAdd}: NewItemFormProps) => {
-    const [text, setText] = useState("")
+export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
+  const [text, setText] = useState("");
+  const inputRef = useFocus();
 
-    return(
-        <NewItemFormContainer>
-            <NewItemInput value={text} onChange={(e)=>setText(e.target.value)}/>
-            <AddItemButton onClick={()=>onAdd(text)}>Create</AddItemButton>
-        </NewItemFormContainer>
-    )
-}
+  return (
+    <NewItemFormContainer>
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <AddItemButton onClick={() => onAdd(text)}>Create</AddItemButton>
+    </NewItemFormContainer>
+  );
+};
