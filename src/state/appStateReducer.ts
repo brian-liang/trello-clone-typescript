4fdb9e7bd55f45cd355d5c9087f8176053a83b76
brian-liang <brian.j.liang@gmail.com>
2023-01-06 15:43:08 -0700

    020 -  Handling the MOVE_LIST action

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -40,5 +40,13 @@ export const appStateReducer = (
       });
       break;
     }
+
+    case "MOVE_LIST": {
+      const { draggedId, hoverId } = action.payload;
+      const dragIndex = findItemIndexById(draft.lists, draggedId);
+      const hoverIndex = findItemIndexById(draft.lists, hoverId);
+      draft.lists = moveItem(draft.lists, dragIndex, hoverIndex);
+      break;
+    }
   }
 };
